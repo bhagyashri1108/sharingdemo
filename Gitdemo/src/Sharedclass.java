94d122ea4d6456b212e1fa3c2a8831d223c510fd
Anshuman0301 <anshuman.ticollege@gmail.com>
2022-08-10 23:27:23 +0530

    Commit

--- Gitdemo/src/Sharedclass.java
+++ Gitdemo/src/Sharedclass.java
@@ -6,6 +6,11 @@ public class Sharedclass {
 		
 		System.out.println("Changes Made By Anshuman");
 		System.out.println("Changes made Again ");
+		
+		int a = 10;
+		int b = 20;
+		int c = a+b;
+		System.out.println(c);   // Practicing Git
 	}
 
 }
